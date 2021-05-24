4db79c70ff109740066bf2c54474904b7d38a972
Karishma Shaik <76698398+karishma283@users.noreply.github.com>
2021-05-24 23:56:35 +0200

    EXERCISE 6

--- d5.js
+++ d5.js
@@ -81,3 +81,18 @@ function strivify(k){
 }
 let test = strivify("Strivehey")
 console.log(test)
+
+
+/* EXERCISE 6
+Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
+HINT: Module Operator
+*/
+
+function check3and7(a){
+    if(a % 3 ==0 || a % 7==0){
+        return "The given number is multipe of 3 or 7"
+    }else{
+        return "The given number is not multipe of 3 or 7"
+    }
+}
+console.log(check3and7(8))
