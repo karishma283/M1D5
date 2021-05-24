d386dbae0f043b8289a28e5bae5833b68b1cc3ab
Karishma Shaik <76698398+karishma283@users.noreply.github.com>
2021-05-24 23:52:40 +0200

    EXERCISE 1

---
+++ d5.js
@@ -0,0 +1,16 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- The solution must be available for the tutors by the end of the day (5PM CET)
+- You can Google / use StackOverflow BUT we suggest you to use just the material provided
+*/
+
+/* EXERCISE 1
+Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
+*/
+const area = function(l1,l2) {
+    return l1 * l2;
+}
+let rectangle=area(100,90)
+console.log("The total associated rectangle area is", rectangle)
